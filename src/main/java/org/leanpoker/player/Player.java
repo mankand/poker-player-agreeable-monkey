b11ed62038fda082e657b78ee010083ef66bdc1d
fabian-froehlich <froehlich.fabian@t-online.de>
2016-05-14 12:59:23 +0200

    yolo

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,20 +1,24 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
 
-import java.util.Map;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-      /*JsonObject jsonRequest = new JsonParser().parse(request);
+    	
+    	JsonObject jsonRequest = request.getAsJsonObject();
+    	JsonArray players = jsonRequest.getAsJsonArray("player");
+    	System.out.println(players);
+    /*
+      JsonObject jsonRequest = (JsonObject) request;
       JsonArray players = jsonRequest.getJsonArray("players");
       JsonObject us = players.getJsonObject(2);
       System.out.println(us);*/
-
 
       return 1000;
     }
