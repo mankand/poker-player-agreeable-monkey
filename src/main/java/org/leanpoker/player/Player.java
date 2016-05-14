b05a9c93f344f449c8c14a6352789b549f8225c9
fabian-froehlich <froehlich.fabian@t-online.de>
2016-05-14 14:29:04 +0200

    yolo

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,41 +4,44 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-
 public class Player {
 
-    static final String VERSION = "Yolo";
-    public static int betRequest(JsonElement request) {
+	static final String VERSION = "Yolo";
 
-    	JsonObject jsonRequest = request.getAsJsonObject();
-    	JsonArray orbits = jsonRequest.getAsJsonArray("orbits");
+	public static int betRequest(JsonElement request) {
 
-    	System.out.println(orbits);
+		JsonObject jsonRequest = request.getAsJsonObject();
+		JsonArray orbits = jsonRequest.getAsJsonArray("orbits");
 
+		System.out.println(orbits);
 
-    	JsonArray players = jsonRequest.getAsJsonArray("players");
-    	JsonElement monkey = players.get(2);
-    	JsonArray cards = ((JsonObject)monkey).getAsJsonArray("hole_cards");
+		JsonArray players = jsonRequest.getAsJsonArray("players");
+		JsonElement monkey = players.get(2);
+		JsonArray cards = ((JsonObject) monkey).getAsJsonArray("hole_cards");
 
-    	JsonObject c1 = (JsonObject)cards.get(0);
-    	JsonObject c2 = (JsonObject)cards.get(1);
+		JsonObject c1 = (JsonObject) cards.get(0);
+		JsonObject c2 = (JsonObject) cards.get(1);
 
-    	/*
-    	if(c1.get("rank").equals(c2.get("rank"))){
-    		return 1000;
-    	}*/
+		/*
+		 * if(c1.get("rank").equals(c2.get("rank"))){ return 1000; }
+		 */
 
-    	System.out.println(cards);
-    	System.out.println(c1.get("rank"));
-    /*
-      JsonObject jsonRequest = (JsonObject) request;
-      JsonArray players = jsonRequest.getJsonArray("players");
-      JsonObject us = players.getJsonObject(2);
-      System.out.println(us);*/
+		System.out.println(cards);
+		System.out.println(c1.get("rank"));
+		/*
+		 * JsonObject jsonRequest = (JsonObject) request; JsonArray players =
+		 * jsonRequest.getJsonArray("players"); JsonObject us =
+		 * players.getJsonObject(2); System.out.println(us);
+		 */
 
-      return 1000;
-    }
+		int randomNum;
+		int minimum = 500;
+		int maximum = 1000;
+		randomNum = minimum + (int) (Math.random() * maximum);
 
-    public static void showdown(JsonElement game) {
-    }
+		return randomNum;
+	}
+
+	public static void showdown(JsonElement game) {
+	}
 }
