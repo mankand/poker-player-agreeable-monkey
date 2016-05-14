8984997021e3c00d092cdc6a13429e0dde3c155d
Maria-Anna Kandsorra <mak@informatik.uni-kiel.de>
2016-05-14 11:41:48 +0200

    Put stuff back

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,6 +2,7 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import javax.json;
 
 import java.util.Map;
 
@@ -10,10 +11,10 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-      /*JsonObject jsonRequest = new JsonParser().parse(request);
+      JsonObject jsonRequest = new JsonParser().parse(request);
       JsonArray players = jsonRequest.getJsonArray("players");
       JsonObject us = players.getJsonObject(2);
-      System.out.println(us);*/
+      System.out.println(us);
 
 
       return 1000;
