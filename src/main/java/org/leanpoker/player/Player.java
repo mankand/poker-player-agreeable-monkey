6ce8c3b401c6faab0cc7c9a387f9cc372bbd5464
Maria-Anna Kandsorra <mak@informatik.uni-kiel.de>
2016-05-14 13:06:32 +0200

    commented out again

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,25 +1,23 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import javax.json;
-
-import java.util.Map;
+import com.google.gson.JsonObject;
 
 public class Player {
-
     static final String VERSION = "Default Java folding player";
-
     public static int betRequest(JsonElement request) {
-      /*JsonObject jsonRequest = new JsonParser().parse(request);
+
+    	JsonObject jsonRequest = request.getAsJsonObject();
+    	JsonArray players = jsonRequest.getAsJsonArray("player");
+    	System.out.println(players);
+    /*
+      JsonObject jsonRequest = (JsonObject) request;
       JsonArray players = jsonRequest.getJsonArray("players");
       JsonObject us = players.getJsonObject(2);
       System.out.println(us);*/
-
-
       return 1000;
     }
-
     public static void showdown(JsonElement game) {
     }
 }
