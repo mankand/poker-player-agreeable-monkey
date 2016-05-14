61ab2e21379550c34bb03246c3f8ee53d4df56a1
Maria-Anna Kandsorra <mak@informatik.uni-kiel.de>
2016-05-14 14:17:28 +0200

    commented out again

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,17 +1,21 @@
 package org.leanpoker.player;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+
 public class Player {
+
     static final String VERSION = "Yolo";
-    static int i = 0;
     public static int betRequest(JsonElement request) {
-    	if(i < 1){
-    		i++;
-    		return 0;
-    	}
 
     	JsonObject jsonRequest = request.getAsJsonObject();
+    	JsonArray orbits = jsonRequest.getAsJsonArray("orbits");
+
+    	System.out.println(orbits);
+
+
     	JsonArray players = jsonRequest.getAsJsonArray("players");
     	JsonElement monkey = players.get(2);
     	JsonArray cards = ((JsonObject)monkey).getAsJsonArray("hole_cards");
@@ -19,7 +23,10 @@ public class Player {
     	JsonObject c1 = (JsonObject)cards.get(0);
     	JsonObject c2 = (JsonObject)cards.get(1);
 
-    	System.out.println(i);
+    	if(c1.get("rank").equals(c2.get("rank"))){
+    		return 1000;
+    	}
+
     	System.out.println(cards);
     	System.out.println(c1.get("rank"));
     /*
@@ -27,8 +34,10 @@ public class Player {
       JsonArray players = jsonRequest.getJsonArray("players");
       JsonObject us = players.getJsonObject(2);
       System.out.println(us);*/
+
       return 1000;
     }
+
     public static void showdown(JsonElement game) {
     }
 }
